a210838d069e19d64e203aac8833be470368992b
ivg1 <ivangarkuhome@outlook.com>
2026-06-19 13:28:11 +0200

    add password eye

--- frontend/src/pages/auth/SignupPage.jsx
+++ frontend/src/pages/auth/SignupPage.jsx
@@ -14,6 +14,8 @@ export default function SignupPage() {
     const [error, setError] = useState("");
     const [showError, setShowError] = useState(true);
 
+    const [shown, setShown] = useState(false);
+
     const handleSubmit = async (e) => {
         e.preventDefault();
 
@@ -41,7 +43,7 @@ export default function SignupPage() {
             setShowError(true);
             return;
         }
-        let characters = /[!@#$%^&*/+-_]/;
+        let characters = /[!@#$%^&*\/+\-_.?]/;
         if (!characters.test(values.password)) {
             console.error("password must include at least 1 symbol");
             setError("Password must include at least 1 symbol (!,@,#,$,%,^,&,*,/,+,-).");
@@ -153,7 +155,23 @@ export default function SignupPage() {
                             <div className="mb-2 block">
                                 <Label htmlFor="password">Password:&nbsp;<span className="text-red-600">*</span></Label>
                             </div>
-                            <TextInput id="password" name="password" type="password" placeholder="••••••••" required shadow />
+                            <div className="relative">
+                                <TextInput id="password" name="password" type={shown ? "text" : "password"} placeholder="••••••••" required shadow />
+                                <div onClick={() => shown ? setShown(false) : setShown(true)} className="absolute inset-y-0 right-2 flex items-center">
+									{shown ? (
+										<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-5">
+											<path d="M3.53 2.47a.75.75 0 0 0-1.06 1.06l18 18a.75.75 0 1 0 1.06-1.06l-18-18ZM22.676 12.553a11.249 11.249 0 0 1-2.631 4.31l-3.099-3.099a5.25 5.25 0 0 0-6.71-6.71L7.759 4.577a11.217 11.217 0 0 1 4.242-.827c4.97 0 9.185 3.223 10.675 7.69.12.362.12.752 0 1.113Z" />
+											<path d="M15.75 12c0 .18-.013.357-.037.53l-4.244-4.243A3.75 3.75 0 0 1 15.75 12ZM12.53 15.713l-4.243-4.244a3.75 3.75 0 0 0 4.244 4.243Z" />
+											<path d="M6.75 12c0-.619.107-1.213.304-1.764l-3.1-3.1a11.25 11.25 0 0 0-2.63 4.31c-.12.362-.12.752 0 1.114 1.489 4.467 5.704 7.69 10.675 7.69 1.5 0 2.933-.294 4.242-.827l-2.477-2.477A5.25 5.25 0 0 1 6.75 12Z" />
+										</svg>
+									) : (
+										<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-5">
+											<path d="M12 15a3 3 0 1 0 0-6 3 3 0 0 0 0 6Z" />
+											<path fillRule="evenodd" d="M1.323 11.447C2.811 6.976 7.028 3.75 12.001 3.75c4.97 0 9.185 3.223 10.675 7.69.12.362.12.752 0 1.113-1.487 4.471-5.705 7.697-10.677 7.697-4.97 0-9.186-3.223-10.675-7.69a1.762 1.762 0 0 1 0-1.113ZM17.25 12a5.25 5.25 0 1 1-10.5 0 5.25 5.25 0 0 1 10.5 0Z" clipRule="evenodd" />
+										</svg>
+									)}
+								</div>
+                            </div>
                             <ul className="list-disc px-4 mt-2 text-gray-400 text-sm">
                                 <li>Must be 8 or more characters</li>
                                 <li>Must include a symbol (!,@,#,$,%,^,&,*,/,+,-)</li>
